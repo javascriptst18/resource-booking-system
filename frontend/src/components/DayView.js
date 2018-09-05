c7a75e5b593236e9e8ed2892397b9f389b86591f
Denizhan Kilic <denizhankilic@gmail.com>
2018-09-05 13:29:17 +0200

    added mongoose and cros to package.json

---
+++ frontend/src/components/DayView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class DayView extends React.Component {
+
+  state = {}
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default DayView;
---
+++ frontend/src/components/OneView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class OneView extends React.Component {
+
+  state = {}
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default OneView;
