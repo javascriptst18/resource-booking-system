bb80dc3cdd6b87b325c3c0c9956b07072801501e
Denizhan Kilic <denizhankilic@gmail.com>
2018-09-06 14:35:03 +0200

    Work in progress: Datepicker

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,47 +1,67 @@
 import React from 'react';
 import LoginForm from './LoginForm';
+import { Container, Menu, Button } from 'semantic-ui-react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import ResourceList from './ResourceList';
+
+import 'react-datepicker/dist/react-datepicker.css';
+import './App.css';
 
 class App extends React.Component {
   state = {
     user: '',
     error: '',
-    isLoading: false
+    isLoading: false,
+    startDate: moment(),
   };
 
   componentDidMount() {
-    const user = localStorage.getItem('user');
-    if (user) {
-      this.setState({ user: JSON.parse(user) });
-    }
+    // const user = localStorage.getItem('user');
+    // if (user) {
+    //   this.setState({ user: JSON.parse(user) });
+    // }
   }
 
-  login = (user) => {
+  login = user => {
     this.setState({ user });
     localStorage.setItem('user', JSON.stringify(user));
-  }
+  };
 
   logout = () => {
-    fetch('/logout')
-      .then(() => {
-        this.setState({ user: '' });
-      })
-  }
+    fetch('/logout').then(() => {
+      this.setState({ user: '' });
+    });
+  };
 
   render() {
-    if (this.state.user) {
-      return (
-        <div className="App container">
-          <button id="logout" onClick={this.logout}>Logout</button>
-          <div><p>Successfully logged in</p></div>
-        </div>
-      );
-    } else {
-      return (
-        <div className="App container">
-          <LoginForm login={this.login}/>
-        </div>
-      );
-    }
+    return (
+      <div>
+        <Menu fixed="top">
+          <Menu.Item position="right">
+            <Button color="blue">Log in</Button>
+          </Menu.Item>
+        </Menu>
+        <Container textAlign="center" style={{ marginTop: '5em' }}>
+          <DatePicker dateFormat="DD MMM YYYY" selected={this.state.startDate} onChange={this.handleChange} />
+        </Container>
+      </div>
+    );
+
+    // if (this.state.user) {
+    //   return (
+    //     <div className="App container">
+    //       <button id="logout" onClick={this.logout}>Logout</button>
+    //       <div><p>Successfully logged in</p></div>
+    //     </div>
+    //   );
+    // } else {
+    //   return (
+    //     <div className="App container">
+    //       <LoginForm login={this.login}/>
+    //     </div>
+    //   );
+    // }
   }
 }
 
