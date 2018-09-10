9712d87d0a3bbf9bf5da1ee48ceb17ec2c4e2e73
Jeremias <40340942+groyper@users.noreply.github.com>
2018-09-11 00:07:16 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    DB_CONN: 'mongodb://jeremias:password01@ds139722.mlab.com:39722/resource-booking-system',
+    PORT: 5000,
+    secret: "3NsdckM6uVTxq1J1mi5ADXFvhrJTu2Sae63CYuvFsKKTSGe7skSqZcfxwvqwVxqqnvyH",
+    refreshTokenSecret: "some-secret-refresh-token-shit",
+    jwtSecret: '3NsdckM6uVTxq1J1mi5ADXFvhrJTu2Sae63CYuvFsKKTSGe7skSqZcfxwvqwVxqqnvyH',
+    jwtSession: {
+        session: false
+    }
+};
