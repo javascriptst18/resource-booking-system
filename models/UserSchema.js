c68a3f10404d245fb415f12e5f4771aaa423b421
Jeremias <40340942+groyper@users.noreply.github.com>
2018-09-11 00:01:54 +0200

    Delete UserSchema.js

--- models/UserSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose'); // Imports mongoose driver for connection to cloud-hosted mongoDB at mlab.com
-
-const UserSchema = new mongoose.Schema({
-  username: String,
-  password: String
-});
-
-module.exports = UserSchema;
