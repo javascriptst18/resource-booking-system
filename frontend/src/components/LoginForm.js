e3a077d1c5ec3aa1307120c54078231155dc2bfc
Jeremias <40340942+groyper@users.noreply.github.com>
2018-09-11 15:41:19 +0200

    JWT-token authentication

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,58 +1,45 @@
 import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react';
+import JWT_Authenticator from './JWT_Authenticator';
+import styles from 'styled-components';
+
+const Wrapper = styles.div`margin: 5rem auto; text-align: center;`;
+const H1 = styles.h1`font-size: 2rem;`;
+const Formbox = styles.form`padding: 2rem; width: 300px; text-align: center; margin: 2rem auto; border: solid black 1px;`;
+const Input = styles.input`margin: 1rem;`;
+const Submit = styles.input`margin: 1rem; background-color: black; color: white; border: solid black 1px; padding: 1rem;`
 
 class LoginForm extends Component {
+    constructor(){
+        super();
+        this.handleChange = this.handleChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.JWT_auth = new JWT_Authenticator();
+    }
+    componentWillMount(){
+        if(this.JWT_auth.loggedIn())
+            this.props.history.replace('/');
+    }
+    render() {
+        return (
 
-  state = {
-    username: '',
-    password: '',
-    error: ''
-  };
+                <Wrapper>
+                    <H1>Login</H1>
+                    <Formbox onSubmit={this.handleFormSubmit}>
+                        <Input placeholder="Email" name="email" type="text" onChange={this.handleChange}/>
+                        <Input placeholder="Password" name="password" type="password" onChange={this.handleChange} />
+                        <Submit value="SUBMIT" type="submit" />
+                    </Formbox>
+                </Wrapper>
 
-  onSubmit = (event) => {
-    event.preventDefault();
-    fetch(`/login`, {
-      method: 'POST',
-      credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username: this.state.username,
-        password: this.state.password
-      })
-    })
-      .then(response => response.json())
-      .then(user => {
+        );
+    }
 
-        this.props.login(user);
-      })
-      .catch(error => {this.setState({ error: 'Invalid username or password' });
-      });
-  };
+    handleFormSubmit(event){
+        event.preventDefault();
+        this.JWT_auth.login(this.state.email,this.state.password).then(response =>{this.props.history.replace('/')})
+    }
 
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  render() {
-    return (
-      <Container fluid textAlign="left" style={{ marginTop: '5em' }}>
-        <form onSubmit={this.onSubmit}>
-          <label htmlFor="username">Username</label>
-          <input type="text" className="form-control" id="username" name="username" placeholder="Enter username" onChange={this.handleChange} value={this.state.email}/>
-          <p>{this.state.error}</p>
-
-          <label htmlFor="password">Password</label>
-          <input type="password" className="form-control" id="password" name="password" placeholder="Password" onChange={this.handleChange} value={this.state.password}/>
-          <p>{this.state.error}</p>
-
-          <button type="submit">Submit</button>;
-        </form>
-      </Container>
-  )
-    ;
-  }
+    handleChange(event){this.setState({[event.target.name]: event.target.value })}
 }
 
 export default LoginForm;
