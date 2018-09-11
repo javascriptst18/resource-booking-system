d372d4d2afbc7c29024f1fca63ad3bcc280c882c
Denizhan Kilic <denizhankilic@gmail.com>
2018-09-11 13:25:04 +0200

    Added search icon to navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,16 +1,14 @@
 // This component generates the fixed navigation bar on the top of the page
 import React from 'react';
-import { Menu, Button } from 'semantic-ui-react';
+import { Menu, Button, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 // The Button component will render the login form
 function NavBar(props) {
   return (
-    <Menu borderless fixed="top">
-      <Menu.Item>
-        <h3>
-          <i>Thing Booker</i>
-        </h3>
+    <Menu  fixed="top">
+      <Menu.Item icon position="left">
+        <Icon name="search" size="big" />
       </Menu.Item>
       <Menu.Item position="right">
         <Link to="/login">
