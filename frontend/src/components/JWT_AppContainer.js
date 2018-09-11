d004961369b5ff228b1e504fd47eec7e25111b5b
Jeremias <40340942+groyper@users.noreply.github.com>
2018-09-11 15:47:26 +0200

    Render nothing if user = false

--- frontend/src/components/JWT_AppContainer.js
+++ frontend/src/components/JWT_AppContainer.js
@@ -2,42 +2,29 @@ import React, { Component } from 'react';
 import JWT_Authenticator from './JWT_Authenticator';
 
 export default function JWT_AppContainer(JWT_protected_component) {
-
     const JWT_auth = new JWT_Authenticator('http://localhost:5000');
 
     return class JWT_wrapped_component extends Component {
-
         constructor() {
             super();
             this.state = {user: null}
         }
+
         componentWillMount() {
-            if (!JWT_auth.loggedIn()) {
-                this.props.history.replace('/login')
-            }
-            else {
-                try {
-                    const profile = JWT_auth.getProfile()
-                    this.setState({
-                        user: profile
-                    })
+            if (!JWT_auth.loggedIn()) {this.props.history.replace('/login')}
+            else {try {const profile = JWT_auth.getProfile();
+                    this.setState({user: profile})
                 }
-                catch(err){
-                    JWT_auth.logout()
-                    this.props.history.replace('/login')
+                catch(err){JWT_auth.logout();
+                this.props.history.replace('/login');
                 }
             }
         }
-
         render() {
             if (this.state.user) {
-                return (
-                    <JWT_protected_component history={this.props.history} user={this.state.user} />
-                )
+                return (<JWT_protected_component history={this.props.history} user={this.state.user} />)
             }
-            else {
-                return null
-            }
+            else {return null}
         }
     };
 }
