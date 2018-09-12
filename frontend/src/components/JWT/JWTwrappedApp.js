7239d6add757959f73a1d8a30861348a2dcbc286
Jeremias <40340942+groyper@users.noreply.github.com>
2018-09-12 05:05:17 +0200

    Create JWTwrappedApp.js

---
+++ frontend/src/components/JWT/JWTwrappedApp.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import './App.css';
+import JwtAuthenticator from './components/JwtAuthenticator';
+import withAuth from './components/JwtComponentWrapper';
+import styles from 'styled-components';
+
+const Auth = new JwtAuthenticator();
+
+const LogoutButton = styles.button`
+    height: 35px;
+    color: #fff;
+    background-color: #1abc9c;
+    border: none;
+    letter-spacing: 0.2rem;
+    transition: 0.3s opacity ease;
+    cursor: pointer;
+    margin: 1rem;
+}`;
+
+class App extends Component {
+
+    handleLogout() {
+        Auth.logout();
+        this.props.history.replace('/login');
+    }
+
+    render() {
+
+        return (
+            <div className="center">
+                <div className="card">
+                    <h1>Welcome</h1>
+
+                    <LogoutButton type="button" onClick={this.handleLogout.bind(this)}>Logout</LogoutButton>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withAuth(App);
