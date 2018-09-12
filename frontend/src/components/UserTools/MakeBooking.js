1992c5b8d50b45e6038f89a2c140627c238665cb
Denizhan Kilic <denizhankilic@gmail.com>
2018-09-12 23:45:20 +0200

    Added MakeBooking.js

---
+++ frontend/src/components/UserTools/MakeBooking.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+class MakeBooking extends React.Component {
+
+  state = {}
+
+  render() {
+    return <Button>Confirm Booking</Button>;
+  }
+}
+
+export default MakeBooking;
