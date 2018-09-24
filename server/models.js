80938886834cb78ba9f0d7972ae5e99e8b3945bf
Denizhan Kilic <denizhankilic@gmail.com>
2018-09-24 13:58:07 +0200

    Removed unused files

--- server/models.js
+++ server/models.js
@@ -2,30 +2,42 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const User = mongoose.model('User', new Schema({
-  name: String,
-  email: String,
-  password: String,
-}));
+const User = mongoose.model(
+  'User',
+  new Schema({
+    name: String,
+    email: String,
+    password: String,
+  }),
+);
 
-const Resource = mongoose.model('Resource', new Schema({
-  resourceID: Schema.Types.ObjectId,
-  identifier: String,
-  category: String,
-  bookings: [{ type: Schema.Types.ObjectId, ref: 'Booking' }],
-  tags: Array,
-  description: String,
-  availableDays: { type: Array, default: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'] },
-  availableTimes: Array,
-}));
+const Resource = mongoose.model(
+  'Resource',
+  new Schema({
+    resourceID: Schema.Types.ObjectId,
+    identifier: String,
+    category: String,
+    bookings: [{ type: Schema.Types.ObjectId, ref: 'Booking' }],
+    tags: Array,
+    description: String,
+    availableDays: {
+      type: Array,
+      default: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+    },
+    availableTimes: Array,
+  }),
+);
 
-const Booking = mongoose.model('Booking', new Schema({
-  bookingID: Schema.Types.ObjectId,
-  bookingReference: String,
-  name: String,
-  resourceID: { type: Schema.Types.ObjectId, ref: 'Resource' },
-  startTime: { type: Date, required: true },
-  endTime: { type: Date, required: true },
-}));
+const Booking = mongoose.model(
+  'Booking',
+  new Schema({
+    bookingID: Schema.Types.ObjectId,
+    bookingReference: String,
+    name: String,
+    resourceID: { type: Schema.Types.ObjectId, ref: 'Resource' },
+    startTime: { type: Date, required: true },
+    endTime: { type: Date, required: true },
+  }),
+);
 
 module.exports = { User, Resource, Booking };
