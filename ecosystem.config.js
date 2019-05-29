732727e54f6093d3c29480dc27d49b76dab24749
jeremias.hillerberg <jeremias.hillerberg@esss.se>
2019-05-29 14:30:37 +0200

    gcloud

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	apps: [ {
+		name: 'RBS',
+		script: 'server.js',
+		instances: 1,
+		mode: 'fork',
+		autorestart: true,
+		watch: true,
+		ignore_watch: ["./node_modules","./files","./tests","./public"],
+		max_memory_restart: '1G',
+		env: {
+			NODE_ENV: 'dev'
+		},
+		env_prod: {
+			NODE_ENV: 'prod'
+		},
+		env_staging: {
+			NODE_ENV: 'staging'
+		},
+		output: '/usr/src/app/files/pm2/out.log',
+		error: '/usr/src/app/files/pm2/error.log',
+		log_type: 'json',
+		log_date_format: 'YYYY-MM-DD HH:mm Z'
+	} ],
+};
